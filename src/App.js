95de2df24c7c8e2844a762044e54e97292ea8cd5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-08 19:41:08 +0600

    🛠changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Container } from "./Style";
 
 function App() {
+  const [option, setOption] = useState(3);
+  const [billingType, setBillingType] = useState(0);
+
+  const VALUES = [
+    {
+      views: "10K",
+      price: 3,
+    },
+    {
+      views: "50K",
+      price: 12,
+    },
+    {
+      views: "100K",
+      price: 16,
+    },
+    {
+      views: "500K",
+      price: 24,
+    },
+    {
+      views: "1M",
+      price: 36,
+    },
+  ];
+  console.log(VALUES, option, billingType);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <header></header>
+      <main>
+        <input
+          type="range"
+          min="1"
+          max="5"
+          value="3"
+          step="1"
+          onChange={(e) => setOption(e.target.value)}
+        />
+      </main>
+    </Container>
   );
 }
 
